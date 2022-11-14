f5e32c470a91ecc7dc606a2c6ffb97b31b7c5d0b
Ricardo <ricardosousa2709y@gmail.com>
2022-11-14 16:26:50 -0300

    Create aula02.js

---
+++ Primeiros passos JS/aula02.js
@@ -0,0 +1,13 @@
+const etanol = 4.89
+const gasolina = 5.64
+let combustivel = ;
+let distviagem = 364;
+const kmlitro = 12;
+if (combustivel == etanol) {
+    const viagem = ((distviagem/kmlitro) * etanol);
+    console.log ("O valor gasto na viagem é de R$", viagem.toFixed(2));
+} else {
+    const viagem === ((distviagem/kmlitro) * gasolina);
+    console.log ("O valor gasto na viagem é de R$", viagem.toFixed(2));
+}
+
