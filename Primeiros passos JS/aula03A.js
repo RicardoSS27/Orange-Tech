1c6b8b403ebba4c8b300bf1f34207c3f22da318b
Ricardo Sousa <ricardosousa2709y@gmail.com>
2022-11-15 20:30:49 -0300

    Update aula03A.js

--- Primeiros passos JS/aula03A.js
+++ Primeiros passos JS/aula03A.js
@@ -4,21 +4,21 @@ const altura = 1.70;
 let IMC = peso / (altura * altura)
 if ( IMC < 18.5 ) {
     console.log("Você está abaixo do peso ideal!")
-    console.log(IMC)
+    console.log(IMC.toFixed(0))
 } 
-else if ( 18.5 >= IMC < 25 ) {
+else if ( IMC >= 18.5 && IMC < 25 ) {
     console.log("Você está com o peso ideal!")
-    console.log(IMC)
+    console.log(IMC.toFixed(0))
 }
-else if ( 25 >= IMC <= 30 ) {
+else if ( IMC >= 25 && IMC < 30 ) {
     console.log("Você está acima do peso ideal!")
-    console.log(IMC)
+    console.log(IMC.toFixed(0))
 }
-else if ( 30 >= IMC <= 40 ) {
+else if ( IMC >= 30 && IMC < 40 ) {
     console.log("Você está com obesidade !")
-    console.log(IMC)
+    console.log(IMC.toFixed(0))
 }
 else {
     console.log("Você está com obesidade mórbida !")
-    console.log(IMC)
+    console.log(IMC.toFixed(0))
 }
