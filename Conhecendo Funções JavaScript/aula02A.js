a9f5a31289074feb6a2551bf6d10e0833f008ee0
Ricardo Sousa <ricardosousa2709y@gmail.com>
2022-11-16 21:28:14 -0300

    Update aula02A.js

--- Conhecendo Funções JavaScript/aula02A.js
+++ Conhecendo Funções JavaScript/aula02A.js
@@ -1,5 +1,5 @@
 const valorproduto = 5;
-const formapag = "Parcelado2x";
+const formapag = "Débito";
 
 
 function aplicardesconto(valorproduto , desconto) {
@@ -9,17 +9,19 @@ function aplicardesconto(valorproduto , desconto) {
 function aplicarjuros(valorproduto , juros) {
     return preço = valorproduto + (valorproduto * (valorproduto * juros/100))
 }
-
-if (formapag === "Débito") {
-    console.log("O valor a ser pago é R$" , aplicardesconto(5 , 10).toFixed(2));
+function Valorpago(preço) {
+    if (formapag === "Débito") {
+        console.log("O valor a ser pago é R$" , aplicardesconto(5 , 10).toFixed(2));
+    }
+    else if (formapag === "Pix" || formapag === "Dinheiro") {
+        console.log("O valor a ser pago é R$" , aplicardesconto( 5 , 15).toFixed(2));
+    }
+    else if (formapag === "Parcelado2x") {
+        console.log("O valor a ser pago é R$" , valorproduto.toFixed(2));
+    } 
+    else if (formapag === "Parcelado+2x") {
+        console.log("O valor a ser pago é R$" , aplicarjuros( 5 , 10).toFixed(2));
+    }
+    
 }
-else if (formapag === "Pix" || formapag === "Dinheiro") {
-    console.log("O valor a ser pago é R$" , aplicardesconto( 5 , 15).toFixed(2));
-}
-else if (formapag === "Parcelado2x") {
-    console.log("O valor a ser pago é R$" , valorproduto.toFixed(2));
-} 
-else if (formapag === "Parcelado+2x") {
-    console.log("O valor a ser pago é R$" , aplicarjuros( 5 , 10).toFixed(2));
-}
-
+Valorpago();
