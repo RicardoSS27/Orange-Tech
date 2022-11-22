52abd0e3527aa8708bdd2def413d770879d2e709
Ricardo <ricardosousa2709y@gmail.com>
2022-11-22 16:43:59 -0300

    at

--- Arrays e Estruturas de Repetição em JavaScript/aula01.js
+++ Arrays e Estruturas de Repetição em JavaScript/aula01.js
@@ -1,15 +1,5 @@
-const notas = [];
-
-notas.push(5);
-notas.push(7);
-notas.push(8);
-notas.push(2);
-notas.push(5);
-notas.push(8);
-
-const soma = notas[1] + notas[2] + notas[3] + notas[4];
 
 for (let i = 0; i < 10; i++) {
-    const element = array[i];
+    console.log(i);
     
 }
