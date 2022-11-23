da211ba6917ceff1fed1bd585f4ed22e7408939a
Ricardo <ricardosousa2709y@gmail.com>
2022-11-23 13:43:58 -0300

    Atualização

--- Arrays e Estruturas de Repetição em JavaScript/aula01.js
+++ Arrays e Estruturas de Repetição em JavaScript/aula01.js
@@ -1,5 +1,21 @@
+///const nome = "Ricardo Sousa de Sá"
+///for (let i = 0; i < nome.length; i++) {
+///    const letra = nome[i];
+///    console.log(letra)
+///}
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-    
+const notas = [];
+
+notas.push(5);
+notas.push(8);
+notas.push(7);
+notas.push(10);
+
+let soma = 0
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+    media = soma / notas.length;
 }
+console.log(media.toFixed(2));
---
+++ Arrays e Estruturas de Repetição em JavaScript/aula02A.js
@@ -0,0 +1,7 @@
+const numeros = [5,4,3,6,8,12,46,20,21]
+
+for (let i = 0; i < numeros.length; i++) {
+    console.log(i)
+    
+}
+
