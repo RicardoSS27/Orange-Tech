45078259abf61b3d870b9bb1780ccd76f48e3f81
Ricardo <ricardosousa2709y@gmail.com>
2022-11-24 11:07:48 -0300

    Atualização

--- Arrays e Estruturas de Repetição em JavaScript/aula02A.js
+++ Arrays e Estruturas de Repetição em JavaScript/aula02A.js
@@ -1,7 +1,10 @@
-const numeros = [5,4,3,6,8,12,46,20,21]
+const numeros = [5,4,3,6,8,12,46,20,21];
 
 for (let i = 0; i < numeros.length; i++) {
-    console.log(i)
-    
+    const numero = numeros[i];
+
+    if (numero % 2 === 0){
+        console.log(numero);
+    }
 }
 
